0306d9eee86a31e83b999cd8dc000b36307d06bb
cailonghao <1719341471@qq.com>
2020-07-20 13:16:13 +0800

    no message

---
+++ src/main/java/com/example/App/SpringbootTemplateApplication.java
@@ -0,0 +1,21 @@
+package com.example.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class SpringbootTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootTemplateApplication.class, args);
+    }
+
+    @RequestMapping("/start")
+    public String start(){
+        return "receive start message";
+    }
+
+}
