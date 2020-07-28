6fea88febc1ba08f2e31b1623fb36b9c8789fa83
cailonghao <1719341471@qq.com>
2020-07-28 16:17:45 +0800

    no message

--- src/main/java/com/example/App/SpringbootTemplateApplication.java
+++ src/main/java/com/example/App/SpringbootTemplateApplication.java
@@ -1,10 +1,12 @@
 package com.example.App;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @SpringBootApplication
 public class SpringbootTemplateApplication {
@@ -14,17 +16,20 @@ public class SpringbootTemplateApplication {
     }
 
     @RequestMapping("/start")
-    public String start(){
+    public String start() {
+        log.info("ask start");
         return "receive start message";
     }
 
     @RequestMapping("/change")
-    public String change(){
+    public String change() {
+        log.info("ask change");
         return "receive start message";
     }
 
     @RequestMapping("/hehe")
-    public String hehe(){
+    public String hehe() {
+        log.info("ask hehe");
         return "receive start message";
     }
 
